fea76abac238e70230ef052afc9b9af5f5030b66
mdeepikamm <140747054+mdeepikamm@users.noreply.github.com>
2023-09-14 10:37:48 +0100

    Adding NewTest1 File

---
+++ src/test/java/Selenium2/NewTest1.java
@@ -0,0 +1,13 @@
+package Selenium2;
+
+import org.testng.annotations.Test;
+@Test
+public class NewTest1 {
+	public void GitTestMethod0() {
+		  System.out.println("GitTestMethod0");
+	}
+	@Test
+	public void GitTestMethod1() {
+		  System.out.println("GitTestMethod1");
+}
+}
